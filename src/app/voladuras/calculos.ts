4f8cb68d1c6714be0adac03452f568e4b0f1fec2
sebas746 <sebas746@hotmail.com>
2019-09-08 15:02:41 -0500

    Primera parte del formulario de voladuras a cielo abierto terminado

--- src/app/voladuras/calculos.ts
+++ src/app/voladuras/calculos.ts
@@ -5,40 +5,227 @@ import { FormBuilder, FormGroup } from '@angular/forms';
     providedIn: 'root'
 })
 export class Calculos {
-    
+
     redondear(values: any[], decimales) {
-        let response = values.map(function(each_element){
+        let response = values.map(function (each_element) {
             return Number(each_element.toFixed(decimales));
         });
 
         return response;
     }
-    
+
+    degrees_to_radians(degrees) {
+        var pi = Math.PI;
+        return degrees * (pi / 180);
+    }
+
     burden(form: FormGroup) {
         let r1 = form.get("diametroBarreno").value * 0.035;
-        let r2 = ((form.get("tipoExplosivo1").value.Densidad * 2 / form.get("tipoRoca").value.Densidad) + 1.8) * (form.get("diametroBarreno").value/25.4*0.3048);
-        let r3 = 0.012*(2*form.get("tipoExplosivo1").value.Densidad / form.get("tipoRoca").value.Densidad + 1.5) *  form.get("diametroBarreno").value;
-        return this.redondear([ r1, r2, r3 ], 2);
+        let r2 = ((form.get("tipoExplosivo1").value.Densidad * 2 / form.get("tipoRoca").value.Densidad) + 1.8) * (form.get("diametroBarreno").value / 25.4 * 0.3048);
+        let r3 = 0.012 * (2 * form.get("tipoExplosivo1").value.Densidad / form.get("tipoRoca").value.Densidad + 1.5) * form.get("diametroBarreno").value;
+        return this.redondear([r1, r2, r3], 2);
     }
 
     burdenCorregido(burden: any[], form: FormGroup) {
         let r1 = burden[0] * form.get("correcionFactoresGeo").value * form.get("correcionNumeroFilas").value * form.get("correccionEstGeologica").value;
-        let r2 = burden[1] * form.get("correcionFactoresGeo").value * form.get("correcionNumeroFilas").value * form.get("correccionEstGeologica").value; 
+        let r2 = burden[1] * form.get("correcionFactoresGeo").value * form.get("correcionNumeroFilas").value * form.get("correccionEstGeologica").value;
         let r3 = burden[2] * form.get("correcionFactoresGeo").value * form.get("correcionNumeroFilas").value * form.get("correccionEstGeologica").value;
-        return this.redondear([ r1, r2, r3 ], 2);
+        return this.redondear([r1, r2, r3], 2);
     }
 
     indiceRigidez(burdenC: any[], form: FormGroup) {
-        let r1 = burdenC[0] * form.get("alturaBanco").value;
-        let r2 = burdenC[1] * form.get("alturaBanco").value; 
-        let r3 = burdenC[2] * form.get("alturaBanco").value;
-        return this.redondear([ r1, r2, r3 ], 2);
+        let r1 = form.get("alturaBanco").value / burdenC[0];
+        let r2 = form.get("alturaBanco").value / burdenC[1];
+        let r3 = form.get("alturaBanco").value / burdenC[2];
+        return this.redondear([r1, r2, r3], 2);
     }
 
     espacimientoOptimo(burden: any[], form: FormGroup) {
         let r1 = 0.043 * form.get("diametroBarreno").value;
-        let r2 = burden[1] * 1.15; 
+        let r2 = burden[1] * 1.15;
         let r3 = (form.get("alturaBanco").value + 7 * burden[2]) / 8;
-        return this.redondear([ r1, r2, r3 ], 2);
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    tacoPromedio(burden: any[], burdenC: any[], form: FormGroup) {
+        let r1 = form.get("diametroBarreno").value * 0.032;
+        let r2 = burden[1] * 0.7;
+        let r3 = burdenC[2] * 0.7;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    distribucionEnergia(tacoP: any[], form: FormGroup) {
+        let r1 = (1 - (tacoP[0] / form.get("alturaBanco").value)) * 100;
+        let r2 = (1 - (tacoP[1] / form.get("alturaBanco").value)) * 100;
+        let r3 = (form.get("alturaBanco").value - tacoP[2]) / form.get("alturaBanco").value * 100;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    sobreperforacion(form: FormGroup) {
+        let r1 = 0.012 * form.get("diametroBarreno").value;
+        let r2 = 0.012 * form.get("diametroBarreno").value;
+        let r3 = 0.012 * form.get("diametroBarreno").value;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    alturaVerticalBarreno(sobrePerfo: any[], form: FormGroup) {
+        let r1 = (form.get("alturaBanco").value / form.get("angulo2Barreno").value) + (1 - (20 / 100) * sobrePerfo[0]);
+        let r2 = form.get("alturaBanco").value + sobrePerfo[1];
+        let r3 = form.get("alturaBanco").value + sobrePerfo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    alturaEfectivaBarreno(sobrePerfo: any[], form: FormGroup) {
+        let r1 = form.get("alturaBanco").value + sobrePerfo[0] / Math.cos(this.degrees_to_radians(form.get("angulo2Barreno").value));
+        let r2 = form.get("alturaBanco").value + sobrePerfo[1] / Math.cos(this.degrees_to_radians(form.get("angulo2Barreno").value));
+        let r3 = form.get("alturaBanco").value + sobrePerfo[2] / Math.cos(this.degrees_to_radians(form.get("angulo2Barreno").value));
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    verificacionDiametro(burdenC: any[], form: FormGroup) {
+        let r1 = 0;
+        let r2 = 0;
+        let r3 = burdenC[2] / (0.012 * (2 * form.get("tipoExplosivo1").value.Densidad / form.get("tipoRoca").value.Densidad + 1.5));
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    longitudExplosivo(tacoP: any[], alturaEfectivaBarreno: any[]) {
+        let r1 = alturaEfectivaBarreno[0] - tacoP[0];
+        let r2 = alturaEfectivaBarreno[1] - tacoP[1];
+        let r3 = alturaEfectivaBarreno[2] - tacoP[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    areaBarreno(form: FormGroup) {
+        let r1 = form.get("diametroBarreno").value / 1000 * form.get("diametroBarreno").value / 1000 * Math.PI / 4;
+        let r2 = form.get("diametroBarreno").value / 1000 * form.get("diametroBarreno").value / 1000 * Math.PI / 4;
+        let r3 = form.get("diametroBarreno").value / 1000 * form.get("diametroBarreno").value / 1000 * Math.PI / 4;
+        return this.redondear([r1, r2, r3], 4);
+    }
+
+    densidadCargaEmulit(form: FormGroup, areaBarreno: any[]) {
+        let r1 = 0.0031415 * form.get("tipoExplosivo1").value.Densidad * form.get("diametroBarreno").value * form.get("diametroBarreno").value / 4;
+        let r2 = 0.0031415 * form.get("tipoExplosivo1").value.Densidad * form.get("diametroBarreno").value * form.get("diametroBarreno").value / 4;
+        let r3 = form.get("tipoExplosivo1").value.Densidad * 0.001 * 100 * 100 * 100 * areaBarreno[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    volumenArrancado(burden: any[], espacimientoOptimo: any[], burdenC: any[], form: FormGroup) {
+        let r1 = burden[0] * espacimientoOptimo[0] * form.get("alturaBanco").value / form.get("angulo2Barreno").value;
+        let r2 = form.get("alturaBanco").value * burdenC[1] * espacimientoOptimo[1];
+        let r3 = form.get("alturaBanco").value * burdenC[2] * espacimientoOptimo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    rendimientoArranque(alturaVerticalBarreno: any[], volumenArrancado: any[]) {
+        let r1 = volumenArrancado[0] / alturaVerticalBarreno[0];
+        let r2 = 0;
+        let r3 = 0;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    masaTiro(volumenArrancado: any[], form: FormGroup) {
+        let r1 = volumenArrancado[0] * form.get("tipoRoca").value.Densidad;
+        let r2 = volumenArrancado[1] * form.get("tipoRoca").value.Densidad;
+        let r3 = volumenArrancado[2] * form.get("tipoRoca").value.Densidad;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    longitudCargaFondo(form: FormGroup) {
+        let r1 = 0.04 * form.get("diametroBarreno").value;
+        let r2 = 0.04 * form.get("diametroBarreno").value;
+        let r3 = 0.04 * form.get("diametroBarreno").value;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    concentracionCargaFondo(form: FormGroup) {
+        let r1 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo2").value.Densidad;
+        let r2 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo2").value.Densidad;
+        let r3 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo2").value.Densidad;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    cargaFondo(longitudCargaFondo: any[], concentracionCargaFondo: any[]) {
+        let r1 = longitudCargaFondo[0] * concentracionCargaFondo[0];
+        let r2 = longitudCargaFondo[1] * concentracionCargaFondo[1];
+        let r3 = longitudCargaFondo[2] * concentracionCargaFondo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    longitudCargaColumna(tacoPromedio: any[], alturaVerticalBarreno: any[], longitudCargaFondo: any[]) {
+        let r1 = alturaVerticalBarreno[0] - tacoPromedio[0] - longitudCargaFondo[0];
+        let r2 = alturaVerticalBarreno[1] - tacoPromedio[1] - longitudCargaFondo[1];
+        let r3 = alturaVerticalBarreno[2] - tacoPromedio[2] - longitudCargaFondo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    concentracionCargaColumna(form: FormGroup) {
+        let r1 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo1").value.Densidad;
+        let r2 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo1").value.Densidad;
+        let r3 = form.get("diametroBarreno").value * form.get("diametroBarreno").value * 0.0031416 / 4 * form.get("tipoExplosivo1").value.Densidad;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    cargaColumna(longitudCargaColumna: any[], concentracionCargaColumna: any[]) {
+        let r1 = longitudCargaColumna[0] * concentracionCargaColumna[0];
+        let r2 = longitudCargaColumna[1] * concentracionCargaColumna[1];
+        let r3 = longitudCargaColumna[2] * concentracionCargaColumna[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    cargaBarreno(cargaColumna: any[], cargaFondo: any[]) {
+        let r1 = cargaColumna[0] + cargaFondo[0];
+        let r2 = cargaColumna[1] + cargaFondo[1];
+        let r3 = cargaColumna[2] + cargaFondo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    consumoEspecifico(cargaBarreno: any[], volumenArrancado: any[]) {
+        let r1 = cargaBarreno[0] / volumenArrancado[0];
+        let r2 = cargaBarreno[1] / volumenArrancado[1];
+        let r3 = cargaBarreno[2] / volumenArrancado[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    pesoExplosivoPorBarreno(cargaBarreno: any[], densidadCargaEmulit: any[], longitudExplosivo: any[]) {
+        let r1 = cargaBarreno[0];
+        let r2 = densidadCargaEmulit[1] * longitudExplosivo[1];
+        let r3 = densidadCargaEmulit[2] * longitudExplosivo[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    factorPotencia1(volumenArrancado: any[], pesoExplosivoPorBarreno: any[]) {
+        let r1 = pesoExplosivoPorBarreno[0] / volumenArrancado[0];
+        let r2 = pesoExplosivoPorBarreno[1] / volumenArrancado[1];
+        let r3 = pesoExplosivoPorBarreno[2] / volumenArrancado[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    factorPotencia2(masaTiro: any[], pesoExplosivoPorBarreno: any[]) {
+        let r1 = pesoExplosivoPorBarreno[0] / masaTiro[0];
+        let r2 = pesoExplosivoPorBarreno[1] / masaTiro[1];
+        let r3 = pesoExplosivoPorBarreno[2] / masaTiro[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    factorPotencia3(masaTiro: any[], pesoExplosivoPorBarreno: any[]) {
+        let r1 = masaTiro[0] / pesoExplosivoPorBarreno[0];
+        let r2 = masaTiro[1] / pesoExplosivoPorBarreno[1];
+        let r3 = masaTiro[2] / pesoExplosivoPorBarreno[2];
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    energiaExplosiva(pesoExplosivoPorBarreno: any[], form: FormGroup) {
+        let r1 = pesoExplosivoPorBarreno[0] * form.get("tipoExplosivo1").value.EnergiaExplosiva / 1000;
+        let r2 = pesoExplosivoPorBarreno[1] * form.get("tipoExplosivo1").value.EnergiaExplosiva / 1000;
+        let r3 = pesoExplosivoPorBarreno[2] * form.get("tipoExplosivo1").value.EnergiaExplosiva / 1000;
+        return this.redondear([r1, r2, r3], 2);
+    }
+
+    factorEnergia(masaTiro: any[], energiaExplosiva: any[]) {
+        let r1 = energiaExplosiva[0] * 1000 / masaTiro[0];
+        let r2 = energiaExplosiva[1] * 1000 / masaTiro[1];
+        let r3 = energiaExplosiva[2] * 1000 / masaTiro[2];
+        return this.redondear([r1, r2, r3], 2);
     }
 }
--- src/app/voladuras/cieloAbierto.component.ts
+++ src/app/voladuras/cieloAbierto.component.ts
@@ -4,67 +4,151 @@ import { Repository } from './repository';
 import { TipoExplosivo } from '../models/tipoExplosivo.model';
 import { Calculos } from './calculos';
 
+declare var $: any;
+
 @Component({
     templateUrl: "cieloAbierto.component.html"
 })
 export class CieloAbiertoComponent implements OnInit {
     cieloAbiertoForm: FormGroup;
     isSubmitted = false;
-    explosivo1: any = {};
+    explosivo1: TipoExplosivo;
 
-    burden: any[] = [0 , 0, 0];
-    burdenCorregido: any[] = [0 , 0, 0];
-    indiceRigidez: any[] = [0 , 0, 0];
-    espaciamientoOptimo: any[] = [0 , 0, 0];
-    
+    //Valores por defecto
+    defExplosivo1: TipoExplosivo;
+
+    burden: any[] = [0, 0, 0];
+    burdenCorregido: any[] = [0, 0, 0];
+    indiceRigidez: any[] = [0, 0, 0];
+    espaciamientoOptimo: any[] = [0, 0, 0];
+    tacoPromedio: any[] = [0, 0, 0];
+    distribucionEnergia: any[] = [0, 0, 0];
+    sobrePerforacion: any[] = [0, 0, 0];
+    alturaVerticalBarreno: any[] = [0, 0, 0];
+    alturaEfecBarreno: any[] = [0, 0, 0];
+    verifDiametro: any[] = [0, 0, 0];
+    longitudExplosivo: any[] = [0, 0, 0];
+    areaBarreno: any[] = [0, 0, 0];
+    densidadCargaEmulit: any[] = [0, 0, 0];
+    pesoExplosivoPorBarreno: any[] = [0, 0, 0];
+    energiaExplosiva: any[] = [0, 0, 0];
+    volumenArrancado: any[] = [0, 0, 0];
+    rendimientoArranque: any[] = [0, 0, 0];
+    masaTiro: any[] = [0, 0, 0];
+    factorPotencia1: any[] = [0, 0, 0];
+    factorPotencia2: any[] = [0, 0, 0];
+    factorPotencia3: any[] = [0, 0, 0];
+    factorEnergia: any[] = [0, 0, 0];
+    longitudCargaFondo: any[] = [0, 0, 0];
+    concentracionCargaFondo: any[] = [0, 0, 0];
+    cargaFondo: any[] = [0, 0, 0];
+    longitudCargaColumna: any[] = [0, 0, 0];
+    concentracionCargaColumna: any[] = [0, 0, 0];
+    cargaColumna: any[] = [0, 0, 0];
+    cargaBarreno: any[] = [0, 0, 0];
+    consumoEspecifico: any[] = [0, 0, 0];
+
     constructor(private formBuilder: FormBuilder, private repo: Repository,
-                private calculos: Calculos) {
+        private calculos: Calculos) {
+                
     }
 
-    ngOnInit() {       
-        this.createForm();   
+
+
+    ngOnInit() {
+        this.defExplosivo1 = this.repo.tipo_explosivos[0];
+        this.createForm();
+
+        $('#tableResult').DataTable({
+            "ordering": false,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
+            "language": {
+                "lengthMenu": "Display _MENU_ records per page",
+                "zeroRecords": "No se han encontrado registros",
+                "info": "Mostrando página _PAGE_ de _PAGES_",
+                "infoEmpty": "No records available",
+                "infoFiltered": "(filtered from _MAX_ total records)",
+                "search": "Buscar:",
+                "paginate": {
+                    "first": "Primera",
+                    "last": "Última",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+
+            }
+        });
     }
 
     get tipo_explosivos(): TipoExplosivo[] {
-        
+
+
         return this.repo.tipo_explosivos;
     }
 
     get tipo_rocas(): TipoExplosivo[] {
-        
         return this.repo.tipo_rocas;
     }
 
     get form() { return this.cieloAbiertoForm.controls; }
 
-    createForm() {
+    createForm() {     
         this.cieloAbiertoForm = this.formBuilder.group({
             tipoExplosivo1: ['', Validators.required],
             tipoExplosivo2: ['', Validators.required],
             tipoRoca: ['', Validators.required],
-            anguloBarreno: ['', Validators.required],
-            angulo2Barreno: ['', Validators.required],
-            diametroBarreno: ['', Validators.required],
-            alturaBanco: ['', Validators.required],
-            duracionProyecto: ['', Validators.required],
-            totalVoladuras: ['', Validators.required],
-            longitudVia: ['', Validators.required],
-            tamanoCantera: ['', Validators.required],
-            volumenSuelto: ['', Validators.required],
-            volumenMovido: ['', Validators.required],
-            distanciaVivienda: ['', Validators.required],
-            correcionFactoresGeo: ['', Validators.required],
-            correcionNumeroFilas: ['', Validators.required],
-            correccionEstGeologica: ['', Validators.required],
+            anguloBarreno: [18, Validators.required],
+            angulo2Barreno: [0.951, Validators.required],
+            diametroBarreno: [101.6, Validators.required],
+            alturaBanco: [11, Validators.required],
+            duracionProyecto: [3, Validators.required],
+            totalVoladuras: [1, Validators.required],
+            longitudVia: [100, Validators.required],
+            tamanoCantera: ['180 X 50 X10.8', Validators.required],
+            volumenSuelto: [97200, Validators.required],
+            volumenMovido: [200000, Validators.required],
+            distanciaVivienda: [150, Validators.required],
+            correcionFactoresGeo: [0.9, Validators.required],
+            correcionNumeroFilas: [0.95, Validators.required],
+            correccionEstGeologica: [1.10, Validators.required],
         });
     }
 
     submit() {
-        
-        this.burden =  this.calculos.burden(this.cieloAbiertoForm);
+
+        this.burden = this.calculos.burden(this.cieloAbiertoForm);
         this.burdenCorregido = this.calculos.burdenCorregido(this.burden, this.cieloAbiertoForm);
         this.indiceRigidez = this.calculos.indiceRigidez(this.burdenCorregido, this.cieloAbiertoForm);
         this.espaciamientoOptimo = this.calculos.espacimientoOptimo(this.burden, this.cieloAbiertoForm);
+        this.tacoPromedio = this.calculos.tacoPromedio(this.burden, this.burdenCorregido, this.cieloAbiertoForm);
+        this.distribucionEnergia = this.calculos.distribucionEnergia(this.tacoPromedio, this.cieloAbiertoForm);
+        this.sobrePerforacion = this.calculos.sobreperforacion(this.cieloAbiertoForm);
+        this.alturaVerticalBarreno = this.calculos.alturaVerticalBarreno(this.sobrePerforacion, this.cieloAbiertoForm);
+        this.alturaEfecBarreno = this.calculos.alturaEfectivaBarreno(this.sobrePerforacion, this.cieloAbiertoForm);
+        this.verifDiametro = this.calculos.verificacionDiametro(this.burdenCorregido, this.cieloAbiertoForm);
+        this.longitudExplosivo = this.calculos.longitudExplosivo(this.tacoPromedio, this.alturaEfecBarreno);
+        this.areaBarreno = this.calculos.areaBarreno(this.cieloAbiertoForm);
+        this.densidadCargaEmulit = this.calculos.densidadCargaEmulit(this.cieloAbiertoForm, this.areaBarreno);        
+        this.volumenArrancado = this.calculos.volumenArrancado(this.burden, this.espaciamientoOptimo, this.burdenCorregido, this.cieloAbiertoForm);
+        this.rendimientoArranque = this.calculos.rendimientoArranque(this.alturaVerticalBarreno, this.volumenArrancado);
+        this.masaTiro = this.calculos.masaTiro(this.volumenArrancado, this.cieloAbiertoForm);
+        this.longitudCargaFondo = this.calculos.longitudCargaFondo(this.cieloAbiertoForm);
+        this.concentracionCargaFondo = this.calculos.concentracionCargaFondo(this.cieloAbiertoForm);
+        this.cargaFondo = this.calculos.cargaFondo(this.longitudCargaFondo, this.concentracionCargaFondo);
+        this.longitudCargaColumna = this.calculos.longitudCargaColumna(this.tacoPromedio, this.alturaVerticalBarreno, this.longitudCargaFondo);
+        this.concentracionCargaColumna = this.calculos.concentracionCargaColumna(this.cieloAbiertoForm);
+        this.cargaColumna = this.calculos.cargaColumna(this.longitudCargaColumna, this.concentracionCargaColumna);
+        this.cargaBarreno = this.calculos.cargaBarreno(this.cargaColumna, this.cargaFondo);
+        this.consumoEspecifico = this.calculos.consumoEspecifico(this.cargaBarreno, this.volumenArrancado);
+        this.pesoExplosivoPorBarreno = this.calculos.pesoExplosivoPorBarreno(this.cargaBarreno, this.densidadCargaEmulit, this.longitudExplosivo);
+        this.factorPotencia1 = this.calculos.factorPotencia1(this.volumenArrancado, this.pesoExplosivoPorBarreno);
+        this.factorPotencia2 = this.calculos.factorPotencia2(this.masaTiro, this.pesoExplosivoPorBarreno);
+        this.factorPotencia3 = this.calculos.factorPotencia3(this.masaTiro, this.pesoExplosivoPorBarreno);
+        this.energiaExplosiva = this.calculos.energiaExplosiva(this.pesoExplosivoPorBarreno, this.cieloAbiertoForm);
+        this.factorEnergia = this.calculos.factorEnergia(this.masaTiro, this.energiaExplosiva);
 
         this.isSubmitted = true;
         if (this.cieloAbiertoForm.invalid) {
